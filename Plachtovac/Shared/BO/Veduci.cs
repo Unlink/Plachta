f76e3cb93ee8ced0afa77506aabec329e97bdcea
Michal Ďuračík <Michal.Duracik@fri.uniza.sk>
2020-06-15 15:46:20 +0200

    Pridanie infomracii ku vedúcemu

--- Plachtovac/Shared/BO/Veduci.cs
+++ Plachtovac/Shared/BO/Veduci.cs
@@ -1,8 +1,13 @@
-﻿namespace Plachtovac.Shared.BO
+﻿using System.Collections.Generic;
+
+namespace Plachtovac.Shared.BO
 {
     public class Veduci
     {
         public string Prezyvka { get; set; }
         public string Povinnosti { get; set; }
+        public string Druzinka { get; set; }
+        public List<string> Ulohy { get; set; }
+        public string Fotka { get; set; }
     }
 }
